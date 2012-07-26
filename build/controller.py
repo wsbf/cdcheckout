f6f0e5ee458e7c0441c9e148dedaebaf88639612
David Cohen <dcohen@clemson.edu>
2012-07-26 16:27:33 -0400

    ttk, controller, TreeView album list added

---
+++ build/controller.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import view
+import sys, os
+sys.path.insert(0,os.path.join(os.path.dirname(__file__), './models'))
+sys.path.insert(0,os.path.join(os.path.dirname(__file__), './includes'))
+import libalbum
+
+class Controller(object):
+	def __init__(self):
+		self.libalbum = libalbum.LibalbumModel()
+		self.view = view.MainView()
+		self.updateAlbumView()
+		self.view.initialize()
+
+
+	
+	def updateAlbumView(self):
+		fieldNames = ("Artist","Album")
+		albums = self.libalbum.searchByRotation(0) # 0 means to be reviewed
+		albumList = [(x[11], x[1]) for x in albums]
+		self.view.setAlbumView(fieldNames, albumList)
+		
+	
+if __name__ == "__main__":
+	c = Controller()
